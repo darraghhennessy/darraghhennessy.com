d22a92f0a828f2c72fc93b0d8c77d1f99a982685
Darragh Hennessy <darraghhennessy@gmail.com>
2021-05-12 16:29:41 +0100

    replaced site with new Tailwind version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
